7bd3be7e840d29e37e12ddbc24034c22aa5aa0b2
Eduard Gomez <eduard.gomez.escandell@gmail.com>
2021-12-06 21:32:47 +0100

    Day 6: Running tests

--- 06/main.c
+++ 06/main.c
@@ -3,7 +3,29 @@
 
 int main()
 {
-	const bool is_test = false;
-	printf("Part 1: %ld\n", SolvePart1(is_test));
-	printf("Part 2: %ld\n", SolvePart2(is_test));
+	const unsigned long test_1_solution = 5934;
+	const unsigned long test_2_solution = 26984457539;
+
+	const unsigned long test_1 = SolvePart1(true);
+	const unsigned long test_2 = SolvePart2(true);
+
+	printf("Running tests:\n");
+	const char * fmt = "Wrong solution for test %d!\n> Expected: %ld\n> Obtained: %ld\n\n";
+	if(test_1 != test_1_solution)
+	{
+		printf(fmt, 1, test_1_solution, test_1);
+	}
+	if(test_2 != test_2_solution)
+	{
+		printf(fmt, 2, test_2_solution, test_2);
+	}
+
+	if(test_1 == test_1_solution && test_2 == test_2_solution)
+	{
+		printf("All tests passed.\n\n");
+	}
+
+	printf("Running with real data:\n");
+	printf("Part 1: %ld\n", SolvePart1(false));
+	printf("Part 2: %ld\n", SolvePart2(false));
 }
