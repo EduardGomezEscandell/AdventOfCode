a1119776638cc64282a69b30cfcfed8914ef0caf
Eduard Gomez <eduard.gomez.escandell@gmail.com>
2021-12-16 00:21:52 +0100

    Day 16: Ready!

---
+++ 16/day16.c
@@ -0,0 +1,19 @@
+#include "day16.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+DEFINE_TEST(1, 1)
+DEFINE_TEST(2, 1)
+
+solution_t SolvePart1(const bool is_test)
+{
+    // FILE * file = GetFile(is_test, 16);
+
+    return is_test;
+}
+
+solution_t SolvePart2(const bool is_test)
+{
+    return is_test;
+}
