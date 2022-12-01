9d5463df909bbe0587def8ffe01389f7e130db65
Edu Gómez Escandell <eduard.gomez.escandell@gmail.com>
2022-12-01 01:41:00 +0100

    gitignoring

--- 2022/coverage.sh
+++ 2022/coverage.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 
-export AOC_2022_ROOT=`pwd`
-export COVERAGE_DIR="${AOC_2022_ROOT}/coverage"
+export AOC_2022_ROOT=${AOC_2022_ROOT:-`pwd`}
+export COVERAGE_DIR="${AOC_2022_ROOT:-AOC_2022_ROOT}/coverage"
 
 mkdir -p "${COVERAGE_DIR}"
 go test -coverpkg=./... -coverprofile="${COVERAGE_DIR}/coverage.out" -covermode=set ./... > /dev/null
 
 
 if [[ -z "${GITHUB_WORKSPACE}" ]] ; then
-    go tool cover -html="${COVERAGE_DIR}/coverage.out"
+    go tool cover -html="${COVERAGE_DIR}/coverage.out" > "$COVERAGE_DIR/coverage.txt"
 else
-    go tool cover -func="${COVERAGE_DIR}/coverage.out"
+    go tool cover -func="${COVERAGE_DIR}/coverage.out"> "$COVERAGE_DIR/coverage.html"
 fi
