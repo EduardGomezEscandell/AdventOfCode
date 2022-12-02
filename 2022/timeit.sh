6277e3277c5b14942ee555dcd9dc018c9104f8ab
Edu Gómez Escandell <eduard.gomez.escandell@gmail.com>
2022-12-02 19:17:41 +0100

    more synching

--- 2022/timeit.sh
+++ 2022/timeit.sh
@@ -6,7 +6,7 @@ ndays=$(${executable} --count-days)
 day=0
 while [[ day -lt ndays ]]; do
     echo "::group::Day ${day}"
-    time ${executable} --day="${day}"
+    time ${executable} --day="${day}" 2>&1
     echo
     echo "::endgroup::"
     ((++day))
