d8560137c27b29c51ad9f12725a5b174ca1e8377
Edu Gómez Escandell <eduard.gomez.escandell@gmail.com>
2022-12-02 19:12:57 +0100

    Cooler logging

--- 2022/timeit.sh
+++ 2022/timeit.sh
@@ -5,8 +5,9 @@ executable=${executable:-"go run main.go"}
 ndays=$(${executable} --count-days)
 day=0
 while [[ day -lt ndays ]]; do
-    time ( ${executable} --day="${day}" 2>&1) 2> /tmp/time
-    cat /tmp/time | grep "real" | sed 's/real\s\(.*\)/time: \1/'
+    echo "::group::Day ${day}"
+    ${executable} --day="${day}" 2>&1
+    echo "::endgroup::"
     echo
     ((++day))
 done
