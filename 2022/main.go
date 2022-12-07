e2602fad5702e88b3567278082c15b66fe914047
Edu Gómez Escandell <eduard.gomez.escandell@gmail.com>
2022-12-07 22:52:02 +0100

    Fixed timer

--- 2022/main.go
+++ 2022/main.go
@@ -68,9 +68,10 @@ func Run(day uint, output string) error {
 	fmt.Fprintf(w, "DAY %d\n", day)
 
 	start := time.Now()
-	defer fmt.Fprintf(w, "Time elapsed: %s\n", time.Since(start))
+	err = entrypoint(w)
+	fmt.Fprintf(w, "Time elapsed: %s\n", time.Since(start))
 
-	return entrypoint(w)
+	return err
 }
 
 func entryPoint(day uint) (func(io.Writer) error, error) {
