298e2c5e613fed5c78f18a9403a084b600bff35d
Edu Gómez Escandell <eduard.gomez.escandell@gmail.com>
2022-12-10 12:01:11 +0100

    Solved

---
+++ 2022/day10/day10_test.go
@@ -0,0 +1,260 @@
+package day10_test
+
+import (
+	"bytes"
+	"context"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/EduardGomezEscandell/AdventOfCode/2022/day10"
+	"github.com/EduardGomezEscandell/AdventOfCode/2022/utils/array"
+	"github.com/EduardGomezEscandell/AdventOfCode/2022/utils/charray"
+	"github.com/EduardGomezEscandell/AdventOfCode/2022/utils/input"
+	"github.com/EduardGomezEscandell/AdventOfCode/2022/utils/testutils"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMain(m *testing.M) {
+	err := testutils.CheckEnv()
+	if err != nil {
+		log.Printf("Setup: %v", err)
+	}
+	r := m.Run()
+	os.Exit(r)
+}
+
+func TestPart1(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		data []string
+		want int
+	}{
+		"empty":           {data: []string{}, want: 0},
+		"partial example": {data: example[:12], want: 420},
+		"example":         {data: example, want: 13140},
+	}
+
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			lines := array.Map(tc.data, input.NewLine)
+			ctx, cancel := context.WithTimeout(context.Background(), 100000*time.Second)
+			defer cancel()
+
+			ch := charray.FromArray(ctx, lines, 0)
+			got, err := day10.Part1(ch)
+
+			require.NoError(t, err)
+			require.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		data []string
+		want string
+	}{
+		"empty": {data: []string{}, want: `#.......................................
+........................................
+........................................
+........................................
+........................................
+........................................`},
+		"example": {data: example, want: `##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....`},
+	}
+
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			lines := array.Map(tc.data, input.NewLine)
+			ctx, cancel := context.WithTimeout(context.Background(), 100000*time.Second)
+			defer cancel()
+
+			ch := charray.FromArray(ctx, lines, 0)
+			got, err := day10.Part2(ch)
+
+			require.NoError(t, err)
+			require.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func TestRealData(t *testing.T) {
+	expected := `Result of part 1: 15220
+Result of part 2:
+###..####.####.####.#..#.###..####..##..
+#..#.#.......#.#....#.#..#..#.#....#..#.
+#..#.###....#..###..##...###..###..#..#.
+###..#.....#...#....#.#..#..#.#....####.
+#.#..#....#....#....#.#..#..#.#....#..#.
+#..#.#....####.####.#..#.###..#....#..#.
+`
+	buff := new(bytes.Buffer)
+
+	err := day10.Main(buff)
+
+	require.NoError(t, err)
+	require.Equal(t, expected, buff.String())
+}
+
+var example = []string{
+	"addx 15",
+	"addx -11",
+	"addx 6",
+	"addx -3",
+	"addx 5",
+	"addx -1",
+	"addx -8",
+	"addx 13",
+	"addx 4",
+	"noop",
+	"addx -1",
+	"addx 5",
+	"addx -1",
+	"addx 5",
+	"addx -1",
+	"addx 5",
+	"addx -1",
+	"addx 5",
+	"addx -1",
+	"addx -35",
+	"addx 1",
+	"addx 24",
+	"addx -19",
+	"addx 1",
+	"addx 16",
+	"addx -11",
+	"noop",
+	"noop",
+	"addx 21",
+	"addx -15",
+	"noop",
+	"noop",
+	"addx -3",
+	"addx 9",
+	"addx 1",
+	"addx -3",
+	"addx 8",
+	"addx 1",
+	"addx 5",
+	"noop",
+	"noop",
+	"noop",
+	"noop",
+	"noop",
+	"addx -36",
+	"noop",
+	"addx 1",
+	"addx 7",
+	"noop",
+	"noop",
+	"noop",
+	"addx 2",
+	"addx 6",
+	"noop",
+	"noop",
+	"noop",
+	"noop",
+	"noop",
+	"addx 1",
+	"noop",
+	"noop",
+	"addx 7",
+	"addx 1",
+	"noop",
+	"addx -13",
+	"addx 13",
+	"addx 7",
+	"noop",
+	"addx 1",
+	"addx -33",
+	"noop",
+	"noop",
+	"noop",
+	"addx 2",
+	"noop",
+	"noop",
+	"noop",
+	"addx 8",
+	"noop",
+	"addx -1",
+	"addx 2",
+	"addx 1",
+	"noop",
+	"addx 17",
+	"addx -9",
+	"addx 1",
+	"addx 1",
+	"addx -3",
+	"addx 11",
+	"noop",
+	"noop",
+	"addx 1",
+	"noop",
+	"addx 1",
+	"noop",
+	"noop",
+	"addx -13",
+	"addx -19",
+	"addx 1",
+	"addx 3",
+	"addx 26",
+	"addx -30",
+	"addx 12",
+	"addx -1",
+	"addx 3",
+	"addx 1",
+	"noop",
+	"noop",
+	"noop",
+	"addx -9",
+	"addx 18",
+	"addx 1",
+	"addx 2",
+	"noop",
+	"noop",
+	"addx 9",
+	"noop",
+	"noop",
+	"noop",
+	"addx -1",
+	"addx 2",
+	"addx -37",
+	"addx 1",
+	"addx 3",
+	"noop",
+	"addx 15",
+	"addx -21",
+	"addx 22",
+	"addx -6",
+	"addx 1",
+	"noop",
+	"addx 2",
+	"addx 1",
+	"noop",
+	"addx -10",
+	"noop",
+	"noop",
+	"addx 20",
+	"addx 1",
+	"addx 2",
+	"addx 2",
+	"addx -6",
+	"addx -11",
+	"noop",
+	"noop",
+	"noop",
+}
