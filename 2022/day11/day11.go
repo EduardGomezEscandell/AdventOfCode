f978cbaf701079debf736566b3ad020a7c4d7407
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2022-12-12 12:07:54 +0100

    Parsing monkeys

--- 2022/day11/day11.go
+++ 2022/day11/day11.go
@@ -4,8 +4,11 @@ package day11
 import (
 	"bufio"
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
+	"strconv"
+	"strings"
 
 	"github.com/EduardGomezEscandell/AdventOfCode/2022/utils/input"
 )
@@ -16,19 +19,38 @@ const (
 )
 
 // Part1 solves the first half of the problem.
-func Part1(in string) (int, error) {
+func Part1(in []Monkey) (int, error) {
 	return 0, nil
 }
 
 // Part2 solves the second half of the problem.
-func Part2(in string) (int, error) {
+func Part2(in []Monkey) (int, error) {
 	return 0, nil
 }
 
 // ------------- Implementation ------------------
 
-// ------------- Here be boilerplate ------------------
-/// ---------- Here be boilerplate ------------------
+type Monkey struct {
+	Id         int
+	Inventory  []int
+	Inspect    func(int) int
+	TestValue  int
+	ThrowTrue  int
+	ThrowFalse int
+}
+
+func NewMonkey(id int) Monkey {
+	return Monkey{
+		Id:         id,
+		Inventory:  []int{},
+		Inspect:    func(int) int { panic(fmt.Sprintf("No function for monkey #%d", id)) },
+		TestValue:  1,
+		ThrowTrue:  id,
+		ThrowFalse: id,
+	}
+}
+
+// ---------- Here be boilerplate ------------------
 
 // Main is the entry point to today's problem solution.
 func Main(stdout io.Writer) error {
@@ -60,24 +82,132 @@ var ReadDataFile = func() ([]byte, error) {
 
 // ParseInput reads the data file and returns the list of
 // reindeers and their calories.
-func ParseInput() (data string, err error) {
+func ParseInput() ([]Monkey, error) {
 	b, err := ReadDataFile()
 	if err != nil {
-		return data, err
+		return nil, err
 	}
 
 	sc := bufio.NewScanner(bytes.NewReader(b))
-	for sc.Scan() {
-		// line := sc.Text()
+	monkeys := []Monkey{}
+	for i := 0; sc.Scan(); i++ {
+		m, err := parseMonkey(i, sc)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing monkey #%d: %v", i, err)
+		}
+		monkeys = append(monkeys, m)
 	}
 
 	if err := sc.Err(); err != nil {
-		return data, err
+		return monkeys, err
 	}
 
-	if len(data) == 0 {
-		return "", nil
+	return monkeys, nil
+}
+
+func nextLine(s *bufio.Scanner) (string, error) {
+	read := s.Scan()
+	if !read {
+		return "", errors.New("Failed to read from scanner")
+	}
+	return s.Text(), nil
+}
+
+func parseMonkey(id int, sc *bufio.Scanner) (Monkey, error) {
+	m := NewMonkey(id)
+	var err error
+
+	// Empty line (title line in first iteration)
+	line := sc.Text()
+
+	// Title line
+	if line == "" {
+		line, err = nextLine(sc)
+	}
+	if err != nil {
+		return m, err
 	}
 
-	return data, nil
+	want := fmt.Sprintf("Monkey %d:", id)
+	if line != want {
+		return m, fmt.Errorf("unexpected line.\nWant: %s\nGot : %s", want, line)
+	}
+
+	if line, err = nextLine(sc); err != nil {
+		return m, err
+	}
+
+	// Filling inventory
+	want = "  Starting items:"
+	if !strings.HasPrefix(line, want) {
+		return m, fmt.Errorf("unexpected line.\nWant: %s.*\nGot : %s", want, line)
+	}
+	for i, s := range strings.Split(line[len(want)+1:], ", ") {
+		item, err := strconv.Atoi(s)
+		if err != nil {
+			return m, fmt.Errorf("unexpected non-integer in position %d: %q.\nWant: %s %%d, %%d ...\nGot : %s", i, s, want, line)
+		}
+		m.Inventory = append(m.Inventory, item)
+	}
+
+	// Operation
+	if line, err = nextLine(sc); err != nil {
+		return m, err
+	}
+	var op rune
+	var val int
+	if _, err := fmt.Sscanf(line, "  Operation: new = old %c %d", &op, &val); err != nil {
+		if _, err := fmt.Sscanf(line, "  Operation: new = old %c old", &op); err != nil {
+			return m, fmt.Errorf("unexpected line.\nWant:   Operation: new = old %%c (%%d|old)\nGot : %s", line)
+		}
+		switch op {
+		case '*':
+			m.Inspect = func(x int) int { return x * x }
+		case '+':
+			m.Inspect = func(x int) int { return x + x }
+		default:
+			return m, fmt.Errorf("unexpected line.\nWant: Operation: new = old [*+] ...\nGot : %s", line)
+		}
+	} else {
+		switch op {
+		case '*':
+			m.Inspect = func(x int) int { return x * val }
+		case '+':
+			m.Inspect = func(x int) int { return x + val }
+		default:
+			return m, fmt.Errorf("unexpected line.\nWant: Operation: new = old [*+] ...\nGot : %s", line)
+		}
+	}
+
+	// Test
+	if line, err = nextLine(sc); err != nil {
+		return m, err
+	}
+	want = "  Test: divisible by %d"
+	if _, err := fmt.Sscanf(line, want, &val); err != nil {
+		return m, fmt.Errorf("unexpected line.\nWant: %s\nGot : %s", want, line)
+	}
+	m.TestValue = val
+
+	// If true
+	if line, err = nextLine(sc); err != nil {
+		return m, err
+	}
+	want = "    If true: throw to monkey %d"
+	if _, err := fmt.Sscanf(line, want, &val); err != nil {
+		return m, fmt.Errorf("unexpected line.\nWant: %s\nGot : %s", want, line)
+	}
+	m.ThrowTrue = val
+
+	// If false
+	if line, err = nextLine(sc); err != nil {
+		return m, err
+	}
+	want = "    If false: throw to monkey %d"
+	if _, err := fmt.Sscanf(line, want, &val); err != nil {
+		return m, fmt.Errorf("unexpected line.\nWant: %s\nGot : %s", want, line)
+	}
+	m.ThrowFalse = val
+
+	return m, nil
 }
--- 2022/day11/day11_test.go
+++ 2022/day11/day11_test.go
@@ -21,14 +21,44 @@ func TestMain(m *testing.M) {
 	os.Exit(r)
 }
 
+func TestReadData(t *testing.T) {
+	testCases := map[string]struct {
+		data []string
+		want []day11.Monkey
+	}{
+		"single":  {data: example[:6], want: exampleMonkeys[:1]},
+		"two":     {data: example[:13], want: exampleMonkeys[:2]},
+		"example": {data: example, want: exampleMonkeys},
+	}
+
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			defer testutils.Backup(&day11.ReadDataFile)()
+			day11.ReadDataFile = func() ([]byte, error) {
+				return []byte(strings.Join(tc.data, "\n")), nil
+			}
+
+			t.Log(strings.Join(tc.data, "\n"))
+			got, err := day11.ParseInput()
+			require.NoError(t, err)
+			require.Equal(t, len(tc.want), len(got))
+			for i := range got {
+				requireEqualMonkeys(t, tc.want[i], got[i])
+			}
+
+		})
+	}
+}
+
 func TestPart1(t *testing.T) {
 	t.Parallel()
 
 	testCases := map[string]struct {
-		data []string
+		data []day11.Monkey
 		want int
 	}{
-		"empty": {data: []string{}, want: 0},
+		"empty": {data: []day11.Monkey{}, want: 0},
 	}
 
 	for name, tc := range testCases {
@@ -36,7 +66,7 @@ func TestPart1(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
-			got, err := day11.Part1(strings.Join(tc.data, "\n"))
+			got, err := day11.Part1(tc.data)
 
 			require.NoError(t, err)
 			require.Equal(t, tc.want, got)
@@ -48,10 +78,10 @@ func TestPart2(t *testing.T) {
 	t.Parallel()
 
 	testCases := map[string]struct {
-		data []string
+		data []day11.Monkey
 		want int
 	}{
-		"empty": {data: []string{}, want: 0},
+		"empty": {data: []day11.Monkey{}, want: 0},
 	}
 
 	for name, tc := range testCases {
@@ -59,7 +89,7 @@ func TestPart2(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
-			got, err := day11.Part1(strings.Join(tc.data, "\n"))
+			got, err := day11.Part2(tc.data)
 
 			require.NoError(t, err)
 			require.Equal(t, tc.want, got)
@@ -77,6 +107,24 @@ Result of part 2: 0
 
 	require.NoError(t, err)
 	require.Equal(t, expected, buff.String())
+}
+
+func requireEqualMonkeys(t *testing.T, m1, m2 day11.Monkey) {
+	t.Helper()
+
+	require.Equalf(t, m1.Id, m2.Id, "Mismatching monkey ids:\n%v\n%v", m1, m2)
+	require.Equalf(t, m1.Inventory, m2.Inventory, "Mismatching monkey inventories:\n%v\n%v", m1, m2)
+	require.Equalf(t, m1.TestValue, m2.TestValue, "Mismatching monkey test values:\n%v\n%v", m1, m2)
+	require.Equalf(t, m1.ThrowTrue, m2.ThrowTrue, "Mismatching monkey throw true:\n%v\n%v", m1, m2)
+	require.Equalf(t, m1.ThrowFalse, m2.ThrowFalse, "Mismatching monkey throw false:\n%v\n%v", m1, m2)
+	for i := -5; i <= 5; i++ {
+		require.Equalf(t, m1.ThrowFalse, m2.ThrowFalse, `Mismatching monkey functions:
+%v
+%v
+Functions are not the same:
+f1(%d) -> %d
+f2(%d) -> %d`, m1, m2, i, m1.Inspect(i), i, m2.Inspect(i))
+	}
 }
 
 var example = []string{
@@ -107,4 +155,39 @@ var example = []string{
 	"  Test: divisible by 17",
 	"    If true: throw to monkey 0",
 	"    If false: throw to monkey 1",
+}
+
+var exampleMonkeys = []day11.Monkey{
+	{
+		Id:         0,
+		Inventory:  []int{79, 98},
+		Inspect:    func(x int) int { return x * 19 },
+		TestValue:  23,
+		ThrowTrue:  2,
+		ThrowFalse: 3,
+	},
+	{
+		Id:         1,
+		Inventory:  []int{54, 65, 75, 74},
+		Inspect:    func(x int) int { return x + 6 },
+		TestValue:  19,
+		ThrowTrue:  2,
+		ThrowFalse: 0,
+	},
+	{
+		Id:         2,
+		Inventory:  []int{79, 60, 97},
+		Inspect:    func(x int) int { return x * x },
+		TestValue:  13,
+		ThrowTrue:  1,
+		ThrowFalse: 3,
+	},
+	{
+		Id:         3,
+		Inventory:  []int{74},
+		Inspect:    func(x int) int { return x + 3 },
+		TestValue:  17,
+		ThrowTrue:  0,
+		ThrowFalse: 1,
+	},
 }
