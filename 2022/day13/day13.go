f46c574b2b29ee61c39670f785a59baee7d4dd26
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2022-12-13 16:26:07 +0100

    Part 1

--- 2022/day13/day13.go
+++ 2022/day13/day13.go
@@ -3,11 +3,16 @@ package day13
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
+	"strings"
 	"time"
 
+	"github.com/EduardGomezEscandell/AdventOfCode/2022/utils/array"
 	"github.com/EduardGomezEscandell/AdventOfCode/2022/utils/channel"
+	"github.com/EduardGomezEscandell/AdventOfCode/2022/utils/charray"
+	"github.com/EduardGomezEscandell/AdventOfCode/2022/utils/fun"
 	"github.com/EduardGomezEscandell/AdventOfCode/2022/utils/input"
 )
 
@@ -18,17 +23,223 @@ const (
 
 // Part1 solves the first half of the problem.
 func Part1(in <-chan input.Line) (int, error) {
-	channel.Exhaust(in)
-	return 0, nil
+	ch := parseInput(in)
+	var idx int
+	r := charray.Reduce(ch, func(acc int, t [2]item) int {
+		idx++
+		if compItems(t[0], t[1]) == gt {
+			return acc
+		}
+		return acc + idx
+	}, 0)
+
+	return r, nil
 }
 
 // Part2 solves the second half of the problem.
 func Part2(in <-chan input.Line) (int, error) {
+	// arr := array.FromChannel(parseInput(in))
+	// var err error
+	// extraInput := []item{nil, nil}
+	// if extraInput[0], err = Parse("[[2]]"); err != nil {
+	// 	return 0, err
+	// }
+	// if extraInput[1], err = Parse("[[6]]"); err != nil {
+	// 	return 0, err
+	// }
+	// arr = append(arr, extraInput)
+
 	channel.Exhaust(in)
 	return 0, nil
 }
 
 // ------------- Implementation ------------------
+type item interface {
+}
+
+type Comp int
+
+const (
+	lt Comp = iota - 1
+	eq
+	gt
+)
+
+func compItems(a, b item) Comp {
+	x, aInt := a.(int)
+	y, bInt := b.(int)
+	u, aList := a.([]item)
+	v, bList := b.([]item)
+
+	switch {
+	case aInt && bInt:
+		return compInts(x, y)
+	case aInt && bList:
+		return compSlices([]item{a}, v)
+	case aList && bInt:
+		return compSlices(u, []item{b})
+	case aList && bList:
+		return compSlices(u, v)
+	}
+
+	errMsg := "invalid arguments:"
+	if !(aInt || aList) {
+		errMsg += "\n- a is not valid token"
+	}
+	if !(bInt || bList) {
+		errMsg += "\n- b is not valid token"
+	}
+	panic(errors.New(errMsg))
+}
+
+func compInts(a, b int) Comp {
+	switch {
+	case a < b:
+		return lt
+	case a > b:
+		return gt
+	default:
+		return eq
+	}
+}
+
+func compSlices(u, v []item) Comp {
+	size := fun.Min(len(u), len(v))
+	for i := 0; i < size; i++ {
+		c := compItems(u[i], v[i])
+		if c != eq {
+			return c
+		}
+	}
+	return compInts(len(u), len(v))
+}
+
+func parseInput(in <-chan input.Line) <-chan [2]item {
+	out := make(chan [2]item, cap(in))
+
+	go func() {
+		defer close(out)
+		for {
+			// Reading first line in pair
+			line1, ok := <-in
+			if !ok {
+				panic(fmt.Errorf("Failed to read first line"))
+			}
+			if err := line1.Err(); err != nil {
+				panic(err)
+			}
+			t1, err := Parse(line1.Str())
+			if err != nil {
+				panic(fmt.Errorf("failed to parse first line: %v", err))
+			}
+
+			// Reading second line in pair
+			line2, ok := <-in
+			if !ok {
+				panic(fmt.Errorf("Failed to read second line"))
+			}
+			if err := line2.Err(); err != nil {
+				panic(err)
+			}
+			t2, err := Parse(line2.Str())
+			if err != nil {
+				panic(fmt.Errorf("failed to parse second line: %v", err))
+			}
+
+			// Channeling
+			out <- [2]item{t1, t2}
+
+			// Reading empty line to know if we're done
+			_, ok = <-in
+			if !ok {
+				return
+			}
+		}
+	}()
+
+	return out
+}
+
+func Parse(s string) (item, error) {
+	last := len(s) - 1
+	if s[0] != '[' || s[last] != ']' {
+		return nil, fmt.Errorf(`wrong format. Expected "[...]", got %q`, s)
+	}
+	var i int
+	t, err := parse(s[1:], &i)
+	if err != nil {
+		return nil, fmt.Errorf("failed parsing string: %v\nPartial result: %s", err, PrettyPrint(t))
+	}
+	return t, nil
+}
+
+func parse(s string, i *int) (item, error) {
+	root := []item{}
+	var num *int
+	for *i < len(s) {
+		char := s[*i]
+		*i++
+		switch char {
+		case '[':
+			if num != nil {
+				return nil, fmt.Errorf("error: string %q, character i=%d: number %d succeeded by ']'. Expected comma or another digit.", s, *i-1, *num)
+			}
+			x, err := parse(s, i)
+			if err != nil {
+				return root, err
+			}
+			root = append(root, x)
+		case ']':
+			if num != nil {
+				root = append(root, *num)
+			}
+			return root, nil
+		case ',':
+			if num != nil {
+				root = append(root, *num)
+			}
+			num = nil
+		default:
+			if num == nil {
+				num = new(int)
+			}
+			d, err := charToInt(char)
+			if err != nil {
+				return root, err
+			}
+			*num = 10*(*num) + d
+		}
+	}
+	return root, errors.New("unexpected end of string without closing ']'")
+}
+
+func nextLine(in <-chan input.Line) (string, bool, error) {
+	r, ok := <-in
+	if !ok {
+		return "", false, nil
+	}
+	return r.Str(), true, r.Err()
+}
+
+func charToInt(c byte) (int, error) {
+	if c > '9' || c < '0' {
+		return 0, fmt.Errorf("Charcter %c (%d) is not an integer!", c, c)
+	}
+	return int(c - '0'), nil
+}
+
+func PrettyPrint(t item) string {
+	x, aInt := t.(int)
+	u, aList := t.([]item)
+
+	switch {
+	case aInt:
+		return fmt.Sprintf("%d", x)
+	case aList:
+		return "[" + strings.Join(array.Map(u, PrettyPrint), ",") + "]"
+	}
+	panic(errors.New("invalid arguments: t is not valid token"))
+}
 
 // ------------- Here be boilerplate ------------------
 
--- 2022/day13/day13_test.go
+++ 2022/day13/day13_test.go
@@ -25,6 +25,32 @@ func TestMain(m *testing.M) {
 	os.Exit(r)
 }
 
+func TestParse(t *testing.T) {
+	testCases := map[string]struct {
+		data string
+	}{
+		"empty":         {data: "[]"},
+		"single":        {data: "[1]"},
+		"couple":        {data: "[1,3]"},
+		"nesting":       {data: "[1,[3,5]]"},
+		"double digits": {data: "[1,[35,7]]"},
+		"example first": {data: "[1,1,3,1,1]"},
+		"example last":  {data: "[1,[2,[3,[4,[5,6,0]]]],8,9]"},
+	}
+
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			input := tc.data
+			tk, err := day13.Parse(input)
+			require.NoError(t, err)
+			got := day13.PrettyPrint(tk)
+			require.Equal(t, tc.data, got)
+		})
+	}
+}
+
 func TestPart1(t *testing.T) {
 	t.Parallel()
 
@@ -32,7 +58,36 @@ func TestPart1(t *testing.T) {
 		data []string
 		want int
 	}{
-		"empty": {data: []string{}, want: 0},
+		"ok":        {data: []string{"[1,2,3,4]", "[1,2,5]"}, want: 1},
+		"not":       {data: []string{"[1,2,3,4]", "[1,2,3]"}, want: 0},
+		"example 1": {data: []string{"[1,1,3,1,1]", "[1,1,5,1,1]"}, want: 1},
+		"example 2": {data: []string{"[1,1,3,1,1]", "[1,1,5,1,1]", "", "[[1],[2,3,4]]", "[[1],4]"}, want: 3},
+		"example 3": {data: []string{"[1,1,3,1,1]", "[1,1,5,1,1]", "", "[[1],[2,3,4]]", "[[1],4]", "", "[9]", "[[8,7,6]]"}, want: 3},
+		"example": {data: []string{
+			"[1,1,3,1,1]",
+			"[1,1,5,1,1]",
+			"",
+			"[[1],[2,3,4]]",
+			"[[1],4]",
+			"",
+			"[9]",
+			"[[8,7,6]]",
+			"",
+			"[[4,4],4,4]",
+			"[[4,4],4,4,4]",
+			"",
+			"[7,7,7,7]",
+			"[7,7,7]",
+			"",
+			"[]",
+			"[3]",
+			"",
+			"[[[]]]",
+			"[[]]",
+			"",
+			"[1,[2,[3,[4,[5,6,7]]]],8,9]",
+			"[1,[2,[3,[4,[5,6,0]]]],8,9]",
+		}, want: 13},
 	}
 
 	for name, tc := range testCases {
@@ -80,7 +135,7 @@ func TestPart2(t *testing.T) {
 }
 
 func TestRealData(t *testing.T) {
-	expected := `Result of part 1: 0
+	expected := `Result of part 1: 5882
 Result of part 2: 0
 `
 	buff := new(bytes.Buffer)
