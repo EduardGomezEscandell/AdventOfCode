8cadfb11a368c60105a68996deaec6e4f32787c7
Edu Gómez Escandell <eduard.gomez.escandell@gmail.com>
2023-01-06 15:41:41 +0100

    Ready to start

---
+++ 2022/day23/day23_test.go
@@ -0,0 +1,69 @@
+package day23_test
+
+import (
+	"bytes"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/EduardGomezEscandell/AdventOfCode/2022/day23"
+	"github.com/EduardGomezEscandell/AdventOfCode/2022/utils/testutils"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMain(m *testing.M) {
+	err := testutils.CheckEnv()
+	if err != nil {
+		log.Printf("Setup: %v", err)
+	}
+	r := m.Run()
+	os.Exit(r)
+}
+
+func TestPart1(t *testing.T) { // nolint: dupl
+	testCases := map[string]struct {
+		want int
+	}{
+		"empty": {want: 1},
+	}
+
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			got, err := day23.Part1()
+
+			require.NoError(t, err)
+			require.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func TestPart2(t *testing.T) { // nolint: dupl
+	testCases := map[string]struct {
+		want int
+	}{
+		"empty": {want: 1},
+	}
+
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			got, err := day23.Part2()
+
+			require.NoError(t, err)
+			require.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func TestRealData(t *testing.T) {
+	expected := `Result of part 1: 1
+Result of part 2: 1
+`
+	buff := new(bytes.Buffer)
+
+	err := day23.Main(buff)
+
+	require.NoError(t, err)
+	require.Equal(t, expected, buff.String())
+}
