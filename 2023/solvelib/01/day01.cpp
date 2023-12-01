de1cad65ff1056dd507ad19232e7f20d0ce61de9
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2023-12-01 10:22:49 +0100

    Part 1

--- 2023/solvelib/01/day01.cpp
+++ 2023/solvelib/01/day01.cpp
@@ -1,9 +1,32 @@
 #include "day01.hpp"
+#include <algorithm>
+#include <format>
 #include <iostream>
+#include <iterator>
+#include <numeric>
+#include <sstream>
+
+bool is_num(char ch) { return ch >= '0' && ch <= '9'; }
 
 std::int64_t Day01::part1() {
-  std::cout << this->input << std::endl;
-  return 1;
+  std::stringstream ss(this->input);
+
+  return std::transform_reduce(
+      std::istream_iterator<std::string>(ss),
+      std::istream_iterator<std::string>{}, 0, std::plus<int>{},
+      [](std::string const line) {
+        // First digit
+        const auto first = std::ranges::find_if(line, is_num);
+        if (first == line.end())
+          throw std::runtime_error(
+              std::format("line {} contains no numbers", line));
+
+        // Last digit
+        auto last = std::find_if(line.rbegin(), std::reverse_iterator(first), is_num);
+
+        // compute
+        return int(*first - '0') * 10 + int(*last - '0');
+      });
 }
 
 std::int64_t Day01::part2() { return 2; }
--- 2023/solvelib/01/day01_test.hpp
+++ 2023/solvelib/01/day01_test.hpp
@@ -4,7 +4,9 @@
 TEST_CASE("Day 1") {
     Day01 solution{};
 
+    solution.load("./data/01/example.txt");
+
     REQUIRE_EQ(solution.day(), 1);
-    REQUIRE_EQ(solution.part1(), 1);
+    REQUIRE_EQ(solution.part1(), 142);
     REQUIRE_EQ(solution.part2(), 2);
 }
