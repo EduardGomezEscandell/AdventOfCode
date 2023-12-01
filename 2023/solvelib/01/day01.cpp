04a04fe97a79db2d85d0a78305e24aa63f7b4177
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2023-12-01 11:05:57 +0100

    Part 2

--- 2023/solvelib/01/day01.cpp
+++ 2023/solvelib/01/day01.cpp
@@ -4,7 +4,9 @@
 #include <iostream>
 #include <iterator>
 #include <numeric>
+#include <ostream>
 #include <sstream>
+#include <utility>
 
 bool is_num(char ch) { return ch >= '0' && ch <= '9'; }
 
@@ -22,11 +24,67 @@ std::int64_t Day01::part1() {
               std::format("line {} contains no numbers", line));
 
         // Last digit
-        auto last = std::find_if(line.rbegin(), std::reverse_iterator(first), is_num);
+        const auto last =
+            std::find_if(line.rbegin(), std::reverse_iterator(first), is_num);
 
         // compute
         return int(*first - '0') * 10 + int(*last - '0');
       });
 }
 
-std::int64_t Day01::part2() { return 2; }
+template <typename Iterator>
+std::pair<Iterator, int> find_number(Iterator begin, Iterator end) {
+  constexpr static std::array numbers{"one", "two",   "three", "four", "five",
+                                      "six", "seven", "eight", "nine"};
+
+  if (begin == end) {
+    return std::make_pair(end, 0);
+  }
+
+  auto inc = 1;
+  Iterator endtext = end;
+  if (begin > end) {
+    // reverse
+    inc = -1;
+    endtext = begin;
+  }
+
+  for (Iterator it=begin; it != end; it += inc) {
+    if (is_num(*it)) {
+      return std::make_pair(it, int(*it - '0'));
+    }
+
+    std::string_view window(it, endtext);
+
+    auto r = std::ranges::find_if(
+        numbers, [window](auto number) { return window.starts_with(number); });
+    if (r == numbers.end()) {
+      continue;
+    }
+
+    return std::make_pair(it, r - numbers.begin() + 1);
+  }
+
+  return std::make_pair(end, 0);
+}
+
+std::int64_t Day01::part2() {
+  std::stringstream ss(this->input);
+
+  return std::transform_reduce(
+      std::istream_iterator<std::string>(ss),
+      std::istream_iterator<std::string>{}, 0, std::plus<int>{},
+      [](std::string const line) {
+        // First digit
+        const auto first = find_number(line.begin(), line.end());
+        if (first.first == line.end())
+          throw std::runtime_error(
+              std::format("line {} contains no numbers", line));
+
+        // Last digit
+        const auto last = find_number(line.end(), first.first-1);
+
+        // compute
+        return first.second * 10 + last.second;
+      });
+}
--- 2023/solvelib/01/day01_test.hpp
+++ 2023/solvelib/01/day01_test.hpp
@@ -4,9 +4,16 @@
 TEST_CASE("Day 1") {
     Day01 solution{};
 
-    solution.load("./data/01/example.txt");
+    SUBCASE("Part 1") {
+        solution.load("./data/01/example1.txt");
+        REQUIRE_EQ(solution.day(), 1);
+    }
 
-    REQUIRE_EQ(solution.day(), 1);
-    REQUIRE_EQ(solution.part1(), 142);
-    REQUIRE_EQ(solution.part2(), 2);
+    SUBCASE("Part 2") {
+        solution.load("./data/01/example2.txt");
+        REQUIRE_EQ(solution.part2(), 281);
+
+         solution.load("./data/01/example3.txt");
+        REQUIRE_EQ(solution.part2(), 33);
+    }
 }
