9d2bd0caa2b50963beae7abd873a68b69c0894a6
Edu Gómez Escandell <edu.gomez.escandell@canonical.com>
2023-12-16 16:01:18 +0100

    Part 2

--- 2023/solvelib/06/day06.cpp
+++ 2023/solvelib/06/day06.cpp
@@ -10,7 +10,58 @@
 #include <execution>
 #include <functional>
 #include <numeric>
-#include <stdexcept>
+#include <string_view>
+
+namespace {
+
+std::uint64_t parse_int_ignore_nonnumeric(std::string_view str) {
+  std::uint64_t x{};
+  for (auto ch : str) {
+    if (ch < '0' || ch > '9') {
+      continue;
+    }
+    x *= 10;
+    x += static_cast<std::uint64_t>(ch - '0');
+  }
+  return x;
+}
+
+std::uint64_t count_wins(std::uint64_t time, std::uint64_t distance) noexcept {
+  auto T = static_cast<double>(time);
+  auto D = static_cast<double>(distance) + 1;
+
+  // Solve quadratic equation:
+  //
+  //    d=t(T - t)
+  //
+  //  > d is the distance+1 (aka the minimum distance to win)
+  //  > T is the race duration
+  //  > t is the charging time.
+  //  The equation simplifies down:
+  //
+  //  t = -(T/2) ± sqrt((T/2)^2 - d)
+
+  double half = T / 2;
+  double delta = std::sqrt(half * half - D);
+  auto min_t = std::uint64_t(half - delta);
+  auto max_t = std::uint64_t(half + delta);
+
+  // These are lower bounds, we may need a correction
+  auto d = min_t * (time - min_t);
+  if (d <= distance) {
+    ++min_t;
+  }
+
+  d = max_t * (time - max_t);
+  if (d <= distance) {
+    --max_t;
+  }
+
+  xlog::debug("Race {},{} -> [{}, {}]", time, distance, int(min_t), int(max_t));
+  return max_t - min_t + 1;
+}
+
+} // namespace
 
 std::uint64_t Day06::part1() {
   xmas::views::linewise lines(input);
@@ -22,44 +73,17 @@ std::uint64_t Day06::part1() {
   assert(times.size() == distances.size());
 
   // It's faster with std::execution::unseq than with std::execution::par_unseq.
-  // Probably because the input is too small to justify allocating extra threads.
+  // Probably because the input is too small to justify allocating extra
+  // threads.
   return std::transform_reduce(
       std::execution::unseq, times.cbegin(), times.cend(), distances.cbegin(),
-      std::uint64_t{1}, std::multiplies<std::uint64_t>{},
-      [](std::uint64_t time, std::uint64_t distance) {
-        auto T = static_cast<float>(time);
-        auto D = static_cast<float>(distance) + 1;
-
-        // Solve quadratic equation:
-        //
-        //    d=t(T - t)
-        //
-        //  > d is the distance+1 (aka the minimum distance to win)
-        //  > T is the race duration
-        //  > t is the charging time.
-        //  The equation simplifies down:
-        //
-        //  t = -(T/2) ± sqrt((T/2)^2 - d)
-
-        float half = T / 2;
-        float delta = std::sqrt(half * half - D);
-        auto min_t = uint(half - delta);
-        auto max_t = uint(half + delta);
-
-        // These are lower bounds, we may need a correction
-        auto d = min_t * (time - min_t);
-        if (d <= distance) {
-          ++min_t;
-        }
-
-        d = max_t * (time - max_t);
-        if (d <= distance) {
-          --max_t;
-        }
-
-        xlog::debug("Race {},{} -> [{}, {}]", T, D, int(min_t), int(max_t));
-        return max_t - min_t + 1;
-      });
+      std::uint64_t{1}, std::multiplies<std::uint64_t>{}, count_wins);
 }
 
-std::uint64_t Day06::part2() { throw std::runtime_error("Not implemented"); }
+std::uint64_t Day06::part2() {
+  xmas::views::linewise lines(input);
+  auto it = lines.begin();
+  auto time = parse_int_ignore_nonnumeric(*it++);
+  auto distance = parse_int_ignore_nonnumeric(*it);
+  return count_wins(time, distance);
+}
--- 2023/solvelib/06/day06_test.hpp
+++ 2023/solvelib/06/day06_test.hpp
@@ -14,14 +14,14 @@ TEST_CASE("Day 06") {
     Day06 solution{};
     solution.set_input("./data/06/example.txt");
     solution.load();
-    REQUIRE_THROWS(solution.part2());
+    REQUIRE_EQ(solution.part2(), 71503);
   }
 
   SUBCASE("Real data") {
     Day06 solution{};
     solution.set_input("./data/06/input.txt");
     solution.load();
-    REQUIRE_THROWS(solution.part1());
-    REQUIRE_THROWS(solution.part2());
+    REQUIRE_EQ(solution.part1(), 588588);
+    REQUIRE_EQ(solution.part2(), 34655848);
   }
 }
